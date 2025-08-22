1fdcaf9f9e90c241d4771a6bb64279669dfd2194
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-22 11:33:00 -0300

    criando o global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,58 @@
-import { useState, useEffect } from "react"
+import {useState,useEffect} from "react"
 import Header from "./components/Header"
 import Resultado from "./components/Resultado"
 import "./css/global.css"
 import "./css/estilo.css"
 
-
 function App() {
-  
-  //HOCK-useState manipula o estado da variavel
-  const {peso, setPeso} = useState(0);
-  const {altura, setAltura} = useState(0);
-  const {resultado, setResultado} = useState(0);
-  const {mostrarResultado, setMostrarResultado}= useState(false)
-  
-  //FUNÇÃO CAULCULAR IMC
-  const calcularImc = ()=>{
-    const imc = peso/(altura*altura)
+
+  //HOOK-useState - Manipula o estado da variavel
+
+  const [peso,setPeso]=useState(0);
+  const [altura,setAltura]=useState(0);
+  const [resultado,setResultado]=useState(0);
+  const [mostrarresultado, setMostrarResultado]=useState(false)
+
+  //FUNÇÃO CALCULAR IMC
+  const calcularImc=()=>{
+    const imc = peso /(altura*altura)
     return setResultado(imc.toFixed(2))
+
   }
   useEffect(()=>{
-    resultado > 0 ? setMostrarResultado(true): setMostrarResultado(false)
-  }, [resultado])
-  
+   resultado > 0 ? setMostrarResultado(true) : setMostrarResultado(false)  
+  },[resultado])
+
+ 
+
   return (
-   
+    // fragment
     <div className="container">
-    <div className="box">
-    <Header/>
-    <form>
+      <div className="box">
+     <Header/>
+     <form>
       <div>
-        <label htmlFor="altura"><span className="span">(exemplo 1.80)</span></label>
-        <input type="number" id="altura" placeholder="Digite sua altura:" onBlur={({target})=> setAltura(parseFloat(target.value))}></input>
+        <label htmlFor="altura"><span className="span">(exemplo: 1.80)</span></label>
+        <input type="number" id="altura" placeholder="Digite sua Altura"
+        onBlur={({target})=>setAltura(parseFloat(target.value))}/>
       </div>
 
-       <div>
-        <label htmlFor="peso"><span className="span">(exemplo 50 kg)</span></label>
-        <input type="number" id="peso" placeholder="Digite seu peso:" onBlur={({target})=> setPeso(parseFloat(target.value))}></input>
+      <div>
+        <label htmlFor="peso"><span className="span">(exemplo: 80)</span></label>
+        <input type="number" id="peso" placeholder="Digite seu Peso"
+        onBlur={({target})=>setPeso(parseFloat(target.value))}/>
       </div>
+
       <button onClick={calcularImc}>Calcular</button>
-    </form>
+     </form>
     </div>
-    {mostrarResultado &&(
-      <Resultado resultado={Resultado}/>
+    {/* MOSTRA O RESULTADO AO DIGITAR A ALTURA E O PESO */}
+    {mostrarresultado && (
 
+      <Resultado resultado={resultado}/>
     )}
     </div>
-    
-  
+
   )
 }
 
