9f112b9b0b5df91b9c755fd6dd7fe2e7bf8a44b5
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-22 11:01:07 -0300

    criando a funçao calcular

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Resultado from "./components/Resultado"
+import "./css/global.css"
+import "./css/estilo.css"
+
 
 function App() {
   
-
+  //HOCK-useState manipula o estado da variavel
+  const {peso, setPeso} = useState(0);
+  const {altura, setAltura} = useState(0);
+  const {resultado, setResultado} = useState(0);
+  const {mostrarResultado, setMostrarResultado}= useState(false)
+  
+  //FUNÇÃO CAULCULAR IMC
+  const calcularImc = ()=>{
+    const imc = peso/(altura*altura)
+    return setResultado(imc.toFixed(2))
+  }
+  useEffect(()=>{
+    resultado > 0 ? setMostrarResultado(true): setMostrarResultado(false)
+  }, [resultado])
+  
   return (
    
     <div className="container">
@@ -20,6 +37,7 @@ function App() {
         <label htmlFor="peso"><span className="span">(exemplo 50 kg)</span></label>
         <input type="number" id="peso" placeholder="Digite seu peso:" onBlur={({target})=> setPeso(parseFloat(target.value))}></input>
       </div>
+      <button onClick={calcularImc}>Calcular</button>
     </form>
     </div>
     {mostrarResultado &&(
